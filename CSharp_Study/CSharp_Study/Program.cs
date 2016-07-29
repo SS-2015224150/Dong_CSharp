6fd306d6e5f5159f92988fd8a71b8ec8ddda97c4
SS-2015224150 <supergint@naver.com>
2016-07-29 15:42:31 +0900

    C# 공부 시작

---
+++ CSharp_Study/CSharp_Study/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("마스터 최똥글");
+        }
+    }
+}
