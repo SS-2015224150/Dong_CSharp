85106f7b57eab49f17ccb98560777b6db1fca882
SS-2015224150 <supergint@naver.com>
2016-08-02 14:14:55 +0900

    WPF#1

---
+++ CSharp_Study/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Click += Form1_Click;
+        }
+
+        void Form1_Click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("마우스눌림");
+        }
+    }
+}
